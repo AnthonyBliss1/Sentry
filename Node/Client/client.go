04b7631543649505dd7a95ac67ce496144274153
Anthony Bliss <anthony.bliss276@gmail.com>
2026-04-06 16:13:00 -0700

    added mDNS lookup logic

---
+++ Node/Client/client.go
@@ -0,0 +1,135 @@
+package client
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/hashicorp/mdns"
+)
+
+const (
+	WSService = "_Sentry-Hub-WS._tcp"
+	FSService = "_Sentry-Hub-FS._tcp"
+)
+
+type Websocket struct {
+	Server *http.Server
+
+	URL      string
+	Hostname string
+	Addr     string
+	Port     int
+}
+
+type FileServer struct {
+	Server *http.Server
+
+	URL      string
+	Hostname string
+	Addr     string
+	Port     int
+}
+
+type HubServer struct {
+	WS Websocket
+	FS FileServer
+}
+
+type NodeClient struct {
+	Mu  sync.Mutex
+	Hub HubServer
+}
+
+func init() {
+	// setup logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	slog.SetDefault(logger)
+}
+
+func (n *NodeClient) lookupWS() error {
+	entriesCH := make(chan *mdns.ServiceEntry, 16)
+
+	go func() {
+		for entry := range entriesCH {
+			// frontline check
+			if !strings.Contains(entry.Name, WSService) || entry.Port != 8000 {
+				continue
+			}
+
+			// make sure there is a address
+			if entry.AddrV4 == nil {
+				continue
+			}
+
+			slog.Debug("Hub Server found", "Found WS Entry", entry.Name, "Port", entry.Port)
+
+			ws := Websocket{Hostname: entry.Host, Addr: entry.AddrV4.String(), Port: entry.Port}
+			wsURL := fmt.Sprintf("ws://%s:%d/ws", ws.Addr, ws.Port)
+			ws.URL = wsURL
+
+			// first come first serve (for now, will change to hostname targeting i think later)
+			n.Mu.Lock()
+			if n.Hub.WS == (Websocket{}) {
+				slog.Debug("Stored WS Server")
+				n.Hub.WS = ws
+			}
+			n.Mu.Unlock()
+		}
+	}()
+
+	err := mdns.Lookup(WSService, entriesCH)
+	close(entriesCH)
+	return err
+}
+
+func (n *NodeClient) lookupFS() error {
+	entriesCH := make(chan *mdns.ServiceEntry, 16)
+
+	go func() {
+		for entry := range entriesCH {
+			// frontline check
+			if !strings.Contains(entry.Name, FSService) || entry.Port != 8080 {
+				continue
+			}
+
+			// make sure there is a address
+			if entry.AddrV4 == nil {
+				continue
+			}
+
+			slog.Debug("Hub Server found", "Found FS Entry", entry.Name, "Port", entry.Port)
+
+			fs := FileServer{Hostname: entry.Host, Addr: entry.AddrV4.String(), Port: entry.Port}
+			fsURL := fmt.Sprintf("http://%s:%d", fs.Addr, fs.Port)
+			fs.URL = fsURL
+
+			// first come first serve (for now, will change to hostname targeting i think later)
+			n.Mu.Lock()
+			if n.Hub.FS == (FileServer{}) {
+				slog.Debug("Stored FS Server")
+				n.Hub.FS = fs
+			}
+			n.Mu.Unlock()
+		}
+	}()
+
+	err := mdns.Lookup(FSService, entriesCH)
+	close(entriesCH)
+	return err
+}
+
+func (n *NodeClient) MDNSLookup() error {
+	if err := n.lookupWS(); err != nil {
+		return err
+	}
+
+	if err := n.lookupFS(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- Node/main.go
+++ Node/main.go
@@ -4,24 +4,27 @@ import (
 	"log"
 	"time"
 
+	client "github.com/anthonybliss1/Sentry/Node/Client"
 	video "github.com/anthonybliss1/Sentry/Node/Video"
 )
 
 func main() {
+	foundServers := false
+	node := client.NodeClient{}
+
+	// Look for hub servers until both are found
+	for !foundServers {
+		if err := node.MDNSLookup(); err != nil {
+			log.Fatal(err)
+		}
+
+		node.Mu.Lock()
+		foundServers = node.Hub.WS != (client.Websocket{}) && node.Hub.FS != (client.FileServer{})
+		node.Mu.Unlock()
+	}
+
+	// start recording and creating segments
 	stream, err := video.StartStream()
-	if err != nil {
-		log.Print(err)
-	}
-
-	time.Sleep(5 * time.Second)
-
-	if err := stream.Stop(); err != nil {
-		log.Print(err)
-	}
-
-	time.Sleep(2 * time.Second)
-
-	stream, err = video.StartStream()
 	if err != nil {
 		log.Print(err)
 	}
